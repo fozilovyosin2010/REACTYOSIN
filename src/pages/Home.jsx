e5e223aca29221a563b1ea5ef037b7fbc804e90b
fozilovyosin2010 <umnovanika1@gmail.com>
2026-03-27 17:14:33 +0500

    first

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,72 +2,41 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  const api = "http://37.27.29.18:8001/api/categories";
+  const api = "http://37.27.29.18:8001/api/to-dos";
+  const apiImages = "http://37.27.29.18:8001/images";
 
-  const [data, setData] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  async function gData() {
+  const getData = async () => {
     try {
       const { data } = await axios.get(api);
-      setData(data.data);
+      setUsers(data.data);
     } catch (error) {
       console.error(error);
     }
-  }
-
-  async function pData(obj) {
-    try {
-      // await axios.post(api/{request},request_body);
-      await axios.post(api, obj);
-      gData();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  async function dData(id) {
-    try {
-      // await axios.post(api/{request},request_body);
-      await axios.delete(`${api}?id=${id}`);
-      gData();
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  };
 
   useEffect(() => {
-    gData();
+    getData();
   }, []);
-
-  function handleSub(e) {
-    e.preventDefault();
-
-    const obj = { name: e.target["name"].value };
-
-    pData(obj);
-    e.target["name"].value = "";
-  }
-
-  function handleDel(id) {
-    dData(id);
-    console.log("a");
-  }
   return (
     <div>
-      <form onSubmit={handleSub}>
-        <input type="text" name="name" className="border" />
-        <button>Submit</button>
-      </form>
-      <div>
-        {data.map((e) => {
+      <div className="grid grid-cols-3 gap-3">
+        {users.map((e) => {
           return (
-            <div key={e.id} className="flex">
-              <div>{e.name}</div>
-              <button
-                onClick={() => handleDel(e.id)}
-                className="border border-red-500"
-              >
-                del
-              </button>
+            <div>
+              <div>Name: {e.name}</div>
+              <div>Description: {e.description}</div>
+              <div>
+                {e.images.map((e) => {
+                  return (
+                    <img
+                      className="w-[150px] h-[150px]"
+                      src={`${apiImages}/${e.imageName}`}
+                    />
+                  );
+                })}
+              </div>
             </div>
           );
         })}
